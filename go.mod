e6ef57618ca5954ed7ae61f0f4a19001bb7960ab
杨洋 <you@example.com>
2020-12-15 11:46:02 +0800

    简单的MC框架

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gin-web
 go 1.15
 
 require (
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.6.3
 	github.com/joho/godotenv v1.3.0
 	gorm.io/driver/mysql v1.0.3
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"gin-web/routes"
+	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"os"
+	"runtime"
 )
 
 func main() {
@@ -22,7 +24,24 @@ func main() {
 		port = "8887"
 	}
 
-	r.Run(":" + port)
+	//r.Run(":" + port)
+
+	sysType := runtime.GOOS
+
+	//支持平滑重启
+	if sysType == "linux" {
+		// LINUX系统
+
+		endless.ListenAndServe(":"+port, r)
+	}
+
+	//windows只做开发测试
+	if sysType == "windows" {
+		// windows系统
+
+		r.Run(":" + port)
+
+	}
 
 }
 
