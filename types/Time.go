64aeeb7e860663103f9cb4a696d234a946522016
PeterYangs <904801074@qq.com>
2021-06-28 15:47:53 +0800

    注册接口

--- types/Time.go
+++ types/Time.go
@@ -9,17 +9,14 @@ type Time time.Time
 
 func (t Time) Value() (driver.Value, error) {
 
-	tTime := time.Time(t)
-
-	return tTime.Format("2006-01-02 15:04:05"), nil
-
+	return time.Time(t), nil
 }
 
 func (t Time) MarshalJSON() ([]byte, error) {
 	tTime := time.Time(t)
 	tStr := tTime.Format("2006-01-02 15:04:05") // 设置格式
 
-	// 注意 json 字符串风格要求
+	//// 注意 json 字符串风格要求
 	return []byte("\"" + tStr + "\""), nil
 
 }
