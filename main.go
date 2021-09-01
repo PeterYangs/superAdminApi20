359c7a456b9e5024424a994c15a608c77bce3ce6
PeterYangs <904801074@qq.com>
2021-09-01 12:01:52 +0800

    取消服务安装包

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/PeterYangs/tools/http"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/kardianos/service"
 	"github.com/spf13/cast"
 	"log"
 	"os"
@@ -21,63 +20,6 @@ import (
 	"syscall"
 	"time"
 )
-
-type program struct {
-	sigs chan os.Signal
-
-	cxt context.Context
-
-	cancel context.CancelFunc
-
-	wait *sync.WaitGroup
-
-	httpOk chan bool
-
-	httpFail chan bool
-}
-
-func (p *program) Start(s service.Service) error {
-	log.Println("开始服务")
-	go p.run()
-	return nil
-}
-func (p *program) Stop(s service.Service) error {
-	//log.Println("停止服务")
-
-	//time.Sleep(1*time.Second)
-
-	//if runtime.GOOS == "linux" {
-	//
-	//	pid, err := read.Open("logs/run.pid").Read()
-	//
-	//	if err != nil {
-	//
-	//		return err
-	//	}
-	//
-	//	_, err = gcmd.Command("kill " + string(pid)).Start()
-	//
-	//	if err != nil {
-	//
-	//		return err
-	//	}
-	//
-	//}
-
-	<-p.httpOk
-
-	//等待队列退出
-	p.wait.Wait()
-
-	return nil
-}
-func (p *program) run() {
-	// 这里放置程序要执行的代码……
-
-	//fmt.Println("gg")
-
-	p.serviceStart()
-}
 
 func main() {
 
@@ -96,98 +38,27 @@ func main() {
 
 	httpFail := make(chan bool)
 
-	//go boot(cxt, &wait, httpOk, httpFail)
-
-	//启动http服务
-	//go httpStart(httpFail)
-
-	cfg := &service.Config{
-		Name:        "simple",
-		DisplayName: "a simple service",
-		Description: "This is an example Go service.",
-	}
-	// Interface 接口
-	prg := &program{
-
-		sigs:     sigs,
-		cxt:      cxt,
-		cancel:   cancel,
-		httpOk:   httpOk,
-		httpFail: httpFail,
-		wait:     &wait,
-	}
-	// 构建服务对象
-	s, err := service.New(prg, cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// logger 用于记录系统日志
-	logger, err := s.Logger(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(os.Args) == 2 { //如果有命令则执行
-		err = service.Control(s, os.Args[1])
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else { //否则说明是方法启动了
-		err = s.Run()
-		if err != nil {
-			logger.Error(err)
-		}
-	}
-	if err != nil {
-		logger.Error(err)
-	}
-
-}
-
-func (p *program) serviceStart() {
-
-	//kernel.IdInit()
-	//
-	//sigs := make(chan os.Signal, 1)
-	//
-	////退出信号
-	//signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	//
-	//cxt, cancel := context.WithCancel(context.Background())
-	//
-	//wait := sync.WaitGroup{}
-	//
-	//httpOk := make(chan bool)
-	//
-	//httpFail := make(chan bool)
-	//
-	//go boot(cxt, &wait, httpOk, httpFail)
-	//
-	////启动http服务
-	//go httpStart(httpFail)
-
 	go func() {
 
-		sig := <-p.sigs
+		sig := <-sigs
 
 		fmt.Println()
 		fmt.Println(sig)
 
-		p.cancel()
+		cancel()
 	}()
 
-	go boot(p.cxt, p.wait, p.httpOk, p.httpFail)
+	go boot(cxt, &wait, httpOk, httpFail)
 
 	//启动http服务
-	go httpStart(p.httpFail)
+	go httpStart(httpFail)
 
-	//<-p.httpOk
+	<-httpOk
 
-	//等待队列退出
-	//p.wait.Wait()
+	//等待其他服务退出
+	wait.Wait()
 
-	//fmt.Println("finish")
-
-	p.wait.Wait()
+	fmt.Println("finish")
 
 }
 
