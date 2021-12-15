33d5ac9eaad56f6f5508cdb12aba0e96d6f4c47a
PeterYangs <904801074@qq.com>
2021-12-15 11:43:42 +0800

    时间格式化优化

--- types/Time.go
+++ types/Time.go
@@ -16,6 +16,11 @@ func (t Time) MarshalJSON() ([]byte, error) {
 	tTime := time.Time(t)
 	tStr := tTime.Format("2006-01-02 15:04:05") // 设置格式
 
+	if tStr == "0001-01-01 00:00:00" {
+
+		return []byte("\"" + " " + "\""), nil
+	}
+
 	//// 注意 json 字符串风格要求
 	return []byte("\"" + tStr + "\""), nil
 
